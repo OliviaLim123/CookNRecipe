3cc3205ddc2c2652ca86f5a3829b615a6a38b8b2
OliviaLim123 <131115382+OliviaLim123@users.noreply.github.com>
2024-09-01 18:53:57 +1000

    Created the XCode Project

---
+++ CookNRecipe/CookNRecipe/CookNRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookNRecipeApp.swift
+//  CookNRecipe
+//
+//  Created by Olivia Gita Amanda Lim on 1/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct CookNRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
